abf658236414315328cd74e7a6423bafd1b4c272
Rainer Simon <rainer@rainersimon.io>
2023-10-12 10:56:08 +0200

    Basic ellipse tool with constrain aspect ratio + center vs. bounds feature

--- src/ellipse/tool/RubberbandEllipse.svelte
+++ src/ellipse/tool/RubberbandEllipse.svelte
@@ -15,6 +15,12 @@
 
   let x: number, y: number, w: number, h: number;
 
+  let isShiftPressed = false;
+
+  let isCtrlPressed = false;
+
+  let lastMoveEvent: PointerEvent;
+
   const onPointerDown = (evt: PointerEvent) => {
     origin = transform.elementToImage(evt.offsetX, evt.offsetY);
     anchor = origin;
@@ -25,15 +31,35 @@
     h = 1;
   }
 
-  const onPointerMove = (evt: PointerEvent) => {
+  const updateShape = (maybeEvent?: PointerEvent) => {
+    const evt = maybeEvent || lastMoveEvent;
+
     if (origin) {
       anchor = transform.elementToImage(evt.offsetX, evt.offsetY);
 
-      x = Math.min(anchor[0], origin[0]);
-      y = Math.min(anchor[1], origin[1]);
-      w = Math.abs(anchor[0] - origin[0]);
-      h = Math.abs(anchor[1] - origin[1]);
+      if (isCtrlPressed) {
+        const mw = 2 * Math.abs(anchor[0] - origin[0]);
+        const mh = 2 * Math.abs(anchor[1] - origin[1]);
+
+        w = isShiftPressed ? Math.max(mw, mh) : mw;
+        h = isShiftPressed ? w : mh;
+
+        x = Math.min(anchor[0], origin[0] - w / 2);
+        y = Math.min(anchor[1], origin[1] - h / 2);
+      } else {
+        const mw = Math.abs(anchor[0] - origin[0]);
+        const mh = Math.abs(anchor[1] - origin[1]);
+
+        w = isShiftPressed ? Math.max(mw, mh) : mw;
+        h = isShiftPressed ? w : mh;
+
+        x = Math.min(anchor[0], origin[0]);
+        y = Math.min(anchor[1], origin[1]);
+      }
     }
+
+    if (maybeEvent)
+      lastMoveEvent = maybeEvent;
   }
     
   const onPointerUp = () => {
@@ -57,18 +83,50 @@
 
     origin = null;
     anchor = null;
+
+    lastMoveEvent = undefined;
+  }
+
+  const onKeyDown = (evt: KeyboardEvent) => {
+    if (evt.key === 'Shift') {
+      isShiftPressed = true;  
+      updateShape();  
+    }
+
+    if (evt.key === 'Control') {
+      isCtrlPressed = true;
+      updateShape();
+    }
+  }
+
+  const onKeyUp = (evt: KeyboardEvent) => {
+    if (evt.key === 'Shift') {
+      isShiftPressed = false;
+      updateShape();  
+    }
+
+    if (evt.key === 'Control') {
+      isCtrlPressed = false;
+      updateShape();  
+    }
   }
 
   onMount(() => {
     const svg = container.closest('svg');
 
+    document.addEventListener('keyup', onKeyUp);
+    document.addEventListener('keydown', onKeyDown);
+
     svg.addEventListener('pointerdown', onPointerDown);
-    svg.addEventListener('pointermove', onPointerMove);
+    svg.addEventListener('pointermove', updateShape);
     svg.addEventListener('pointerup', onPointerUp);
 
     return () => {
+      document.removeEventListener('keyup', onKeyUp);
+      document.removeEventListener('keydown', onKeyDown);
+
       svg.removeEventListener('pointerdown', onPointerDown);
-      svg.removeEventListener('pointermove', onPointerMove);
+      svg.removeEventListener('pointermove', updateShape);
       svg.removeEventListener('pointerup', onPointerUp); 
     }
   });
@@ -79,18 +137,18 @@
   class="a9s-annotation a9s-rubberband">
   
   {#if origin}
-    <rect
+    <ellipse 
       class="a9s-outer"
-      x={x} 
-      y={y} 
-      width={w} 
-      height={h} />
+      cx={x + w / 2} 
+      cy={y + h / 2} 
+      rx={w / 2} 
+      ry={h / 2} />
 
-    <rect
+    <ellipse 
       class="a9s-inner"
-      x={x} 
-      y={y} 
-      width={w} 
-      height={h} />
+      cx={x + w / 2} 
+      cy={y + h / 2} 
+      rx={w / 2} 
+      ry={h / 2} />
   {/if}
 </g>
